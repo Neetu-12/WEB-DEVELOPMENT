736e84e69a15ece86de8f6e067e484ef99575f77
Neetu <93900988+Neetu-12@users.noreply.github.com>
2022-02-21 11:32:32 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,20 @@
+function save1(){
+var a=document.getElementById("head1").innerText
+b=Number(a)
+b+=1
+document.getElementById("head1").innerText=b
+}
+
+function save3(){
+    var a1=document.getElementById("head1").innerText
+    b1=Number(a1)
+    b1-=1
+    document.getElementById("head1").innerText=b1
+}
+    
+function save2(){
+    var a1=document.getElementById("head1").innerText
+    b1=Number(a1)
+    b1-=1
+    document.getElementById("head1").innerText=b1=0
+}
